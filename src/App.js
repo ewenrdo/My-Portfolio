1f8aaaec37f771b101757c5c94237539e77db280
ewenrdo <funox.dev@gmail.com>
2026-06-01 21:20:58 +0200

    Suppression de la page "expérience"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Ressources from "./pages/Ressources";
@@ -14,7 +13,6 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/experience" element={<Experience />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/portfolio" element={<Portfolio />} />
                 <Route path="/ressources" element={<Ressources />} />
