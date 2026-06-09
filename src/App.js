afcb120eddb45a69eca9cb67346a721ca5a8aba8
ewenrdo <funox.dev@gmail.com>
2026-06-09 14:57:13 +0200

    safety mode

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Ressources from "./pages/Ressources";
 import Simulateur from "./pages/Simulateur";
 import NotFound from "./pages/NotFound";
 import Maintenance from "./pages/Maintenance";
+import Safety from "./pages/Safety";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
@@ -13,11 +14,14 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/*" element={<Safety />} />
+
+                {/*
                 <Route path="/" element={<Home />} />
                 <Route path="/portfolio" element={<Portfolio />} />
                 <Route path="/ressources" element={<Ressources />} />
                 <Route path="/mcc" element={<Simulateur />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />*/}
             </Routes>
             <Analytics />
             <SpeedInsights />
