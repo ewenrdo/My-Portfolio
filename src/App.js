a29c95899dce3ab3adbb426a7790d2f0d2bb87d5
ewenrdo <contact@ewenrdo.fr>
2026-08-30 13:59:15 +0200

    Suppression des fichiers composants intuiles, déplacement de fichiers

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Ressources from "./pages/Ressources";
-import November21 from "./pages/November21";
 import NotFound from "./pages/NotFound";
 import Simulateur from "./pages/Simulateur";
 import Cooldown from "./pages/Cooldown";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import MaintenanceRessources from "./pages/MaintenanceRessources";
 
 const App = () => {
     return (
